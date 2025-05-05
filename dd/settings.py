1f1d36bef4b0d9a4b30a2f8d4a9f923b6e89a30a
Shilnasajd <115145675+Shilnasajd@users.noreply.github.com>
2025-05-05 14:13:46 +0530

    csrf

--- dd/settings.py
+++ dd/settings.py
@@ -147,4 +147,9 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [ BASE_DIR / 'booking/static' ]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-CORS_ALLOW_ALL_ORIGINS = True
+# CORS_ALLOW_ALL_ORIGINS = True
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+    "http://127.0.0.1:3000",
+]
