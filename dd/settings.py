30a46e6071163e6da8d87218e44d54fad74121f3
Shilnasajd <115145675+Shilnasajd@users.noreply.github.com>
2025-05-05 12:24:28 +0530

    images

--- dd/settings.py
+++ dd/settings.py
@@ -50,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'dd.urls'
@@ -141,3 +142,4 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [ BASE_DIR / 'booking/static' ]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
